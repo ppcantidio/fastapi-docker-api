175180acf1f07a6a98fd67a77a16bc29a70f4f6b
Pedro Cantidio <ppcantidio@gmail.com>
2025-06-15 19:17:44 -0300

    Add /time route to return current time

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
+from datetime import datetime
 
 app = FastAPI()
 
 @app.get("/")
 async def read_root():
     return {"Hello": "World"}
+
+@app.get("/time")
+async def read_time():
+    current_time = datetime.now().strftime("%H:%M:%S")
+    return {"current_time": current_time}
